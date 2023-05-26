cc6979ce5be00efa9bef96237c378b888ecbf68e
Gaurav Mutha <gauravmutha1412@gmail.com>
2023-05-26 18:46:54 +0530

    Makes the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import Coins from './components/Coins';
+import CoinDetails from './components/CoinDetails';
+import Exchanges from './components/Exchanges';
 function App() {
   return (
     <Router>
+    <Header/>
       <Routes>
-        <Route>
-
-        </Route>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coins' element={<Coins/>}/>
+      <Route path='/coin/:id' element={<CoinDetails/>}/>
+      <Route path='/exchanges' element={<Exchanges/>}/>
       </Routes>
     </Router>
   );
